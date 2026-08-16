975e7878c7a83a0cb712997fe7c14ed5917eb02d
gdgai2e-pixel <gdgai2e@gmail.com>
2026-08-16 15:30:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
+const http = require('http');
 const mineflayer = require('mineflayer');
 
+// 1. Создаем веб-сервер для прохождения проверки порта на Render
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end('Огурчик-бот работает круглосуточно!');
+}).listen(process.env.PORT || 3000);
+
+// 2. Функция запуска Майнкрафт-бота
 function createBot() {
     const bot = mineflayer.createBot({
         host: '0gyrs4ik.aternos.me', 
         port: 29093,       
-        username: 'Ogyrchik_AFK', // Имя вашего бота на сервере
-        version: false            // Скрипт сам автоматически определит версию сервера
+        username: 'Ogyrchik_AFK', 
+        version: false // Автоматическое определение версии сервера
     });
 
     bot.on('spawn', () => {
         console.log('Огурчик-бот успешно подключился и держит сервер онлайн!');
-        // Бот подпрыгивает каждые 5 секунд, имитируя активность против АФК-системы
+        // Бот подпрыгивает каждые 5 секунд против АФК-системы
         setInterval(() => {
             bot.setControlState('jump', true);
             setTimeout(() => bot.setControlState('jump', false), 500);
@@ -18,7 +26,7 @@ function createBot() {
     });
 
     bot.on('end', () => {
-        console.log('Бот отключился. Попытка перезапуска через 10 секунд...');
+        console.log('Бот отключился. Повторное подключение через 10 секунд...');
         setTimeout(createBot, 10000);
     });
 
